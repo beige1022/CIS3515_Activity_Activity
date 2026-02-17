f6d5d8b42057e224e5d9b07a02696e2e770e541f
beige <markoguaman45@gmail.com>
2026-02-17 12:22:23 -0500

    Completed all TODO Steps, Program returns text in user selected font size

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
+
 class DisplayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,6 +13,8 @@ class DisplayActivity : AppCompatActivity() {
         // TODO Step 3: Extract transferred value and use for lyricsDisplayView text size
         with (findViewById<TextView>(R.id.lyricsDisplayTextView)) {
 
+            val selectedSize = intent.getIntExtra("TEXT_SIZE", 16)
+            textSize = selectedSize.toFloat()
         }
 
 
